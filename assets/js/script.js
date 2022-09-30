a1d8773b4c0492121adb35d6595e1b9a106d9851
Faith Vaz <faith.m.vaz@gmail.com>
2022-09-29 19:01:48 -0600

    updated movie card info and buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,26 @@
 // step one grab relavent html element
 // var submitbtnEl = $("#search-movie")
 var submitbtnEl = document.querySelector('#search-movie');
-var movieTitleEL = document.querySelector('#Title');
-var movieReleasedEL = document.querySelector('#Released');
-var moviePosterEL = document.querySelector('#Poster');
-var moviePlotEL = document.querySelector('#Plot');
+var searchBarBtn = document.querySelector('.search-bar-button');
+var wishListBtn = document.querySelector('.wishlist-button');
+var movieTitleEL = document.querySelector('.Title');
+var movieReleasedEL = document.querySelector('.Released');
+var moviePosterEL = document.querySelector('.Poster');
+var moviePlotEL = document.querySelector('.Plot');
+var results = document.querySelector('#results');
+var wishlist = document.querySelector('#wishlist');
+var searchMovie = document.querySelector('#search-movie-bar');
 // add event listener to connect the function so it happens
 // submitbtnEl.click(getmoviedata)
 submitbtnEl.addEventListener('click', getmoviedata);
+searchBarBtn.addEventListener('click', searchBarPage);
+wishListBtn.addEventListener('click', wishListPage)
 
 
 // query's citys weather pulls data
   function getmoviedata(event) {
     event.preventDefault();
-   
+    results.setAttribute("style", "display");
     var searchInputEl = $("#search-movie-input")
     var title = searchInputEl.val()
     console.log(title);
@@ -30,7 +37,7 @@ submitbtnEl.addEventListener('click', getmoviedata);
        
         movieTitleEL.textContent = Title;
         movieReleasedEL.textContent = Released;
-        moviePosterEL.textContent = Poster;
+        moviePosterEL.src = Poster;
         moviePlotEL.textContent = Plot;
        
         
@@ -41,7 +48,20 @@ submitbtnEl.addEventListener('click', getmoviedata);
   .catch(err => console.error(err));
     
   }
-  
+
+  function searchBarPage(event) {
+    event.preventDefault();
+    searchMovie.setAttribute("style", "display");
+    results.setAttribute("style", "display:none");
+    wishlist.setAttribute("style", "display:none");
+  }
+
+  function wishListPage(event) {
+    event.preventDefault();
+    searchMovie.setAttribute("style", "display:none");
+    results.setAttribute("style", "display:none");
+    wishlist.setAttribute("style", "display");
+  }
 // jQuery for navbar
 
 $(document).ready(function(){
