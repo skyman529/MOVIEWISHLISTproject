58a8995d2454003db1172fb665f47951d95d4839
JeremyF <jfulbright@gmail.com>
2022-10-01 11:00:40 -0600

    Finished wishlist functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -48,7 +48,7 @@ var moviesObj = []; // populated from localStorage on page init
   .catch(err => console.error(err));
     
   }
-
+// Renders Movie Search or Wishlist from Main Nav
   function searchBarPage(event) {
     event.preventDefault();
     searchMovie.setAttribute("style", "display");
@@ -57,65 +57,91 @@ var moviesObj = []; // populated from localStorage on page init
   }
 
   function wishListPage(event) {
+    console.log("wishlistPage function called");
     event.preventDefault();
+   
     searchMovie.setAttribute("style", "display:none");
     results.setAttribute("style", "display:none");
     wishlist.setAttribute("style", "display");
+    renderWishlist();
   }
-// jQuery for navbar
 
+// jQuery for navbar
 $(document).ready(function(){
   $('.sidenav').sidenav();
 });
 
 
-/// Movie Wishlist Functions to save to localStorage;
-
-function storeWishlist() {
-  moviesObj.push(movieTitle);
-  localStorage.setItem("Wishlist", JSON.stringify(moviesObj));
-}
-
-// called on page load to store city search history from localStorage in var
+// called on page load to poplate Wishlist global object from localStorage
 
 function init() {
  var storedWishlist = JSON.parse( localStorage.getItem('Wishlist'));
-  // TODO: Describe the functionality of the following `if` statement.
-  console.log(storedWishlist);
+  // if 
   if (storedWishlist !== null) {
     moviesObj = storedWishlist;
   }
   
-  // renderWishlist();
 }
-init();
 
 
 
-// // TODO: What is the purpose of this function?
+// Renders Wishlist Items from localStorage and displays in Wishlist Card
 function renderWishlist() {
+ 
   // Clear todoList element and update todoCountSpan
   wishlist.innerHTML = "";
   
   // Render a new li for each wishlist
   for (var i = 0; i < moviesObj.length; i++) {
     var movie = moviesObj[i];
-    console.log(movie);
     var li = document.createElement("li");
-    li.textContent = "movie hard coded";
+    li.textContent = movie;
     li.setAttribute("data-index", i);
+    li.setAttribute("class", "collection-item");
     var button = document.createElement("button");
-    button.textContent = "Clear :heavy_check_mark:";
+    button.textContent = "Remove";
+    button.setAttribute("class", "waves-effect waves-light btn teal lighten-5 deep-purple-text text-darken-2 hoverable");
     li.appendChild(button);
     wishlist.appendChild(li);
   }
+  
 }
-renderWishlist();
 
-// function to save event description on save button click
+// Saves Movie to Wishlist
+function storeWishlist() {
+  moviesObj.push(movieTitle); // adds movie to local object
+  localStorage.setItem("Wishlist", JSON.stringify(moviesObj)); //replaces localStorage wishlist values with our moviesObj
+  
+  // displays wishlist after adding a movie
+  renderWishlist();
+  wishlist.setAttribute("style", "display");
+}
+
+// Removes Movie from Wishlist
+wishlist.addEventListener("click", function(event) {
+  var element = event.target;
+
+  // Checks if element is a button
+  if (element.matches("button") === true) {
+    // Get its data-index value and remove the movie element from the list
+    var index = element.parentElement.getAttribute("data-index");
+    moviesObj.splice(index, 1);
+
+    // Store updated todos in localStorage, re-render the list
+    localStorage.setItem("Wishlist", JSON.stringify(moviesObj));
+    renderWishlist();
+    
+
+  }
+});
+
+init();
+
+// listeners to call functions on Movie Search or Wishlist clicks
 addWishlistBtn.addEventListener('click', storeWishlist);
 searchMovieBtn.addEventListener('click', getmoviedata);
 
+// navigation listeners
 searchBarBtn.addEventListener('click', searchBarPage);
 wishListBtn.addEventListener('click', wishListPage)
 
