a08483d7e13e423d2a8a89f677afd94489385d18
Faith Vaz <faith.m.vaz@gmail.com>
2022-10-01 23:13:48 -0600

    fetched TMDB API and rendered it to carousel. added notes to JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,4 @@
-// step one grab relavent html element
-// var submitbtnEl = $("#search-movie")
+// relavent html elements
 var submitbtnEl = document.querySelector('#search-movie');
 var searchBarBtn = document.querySelector('.search-bar-button');
 var wishListBtn = document.querySelector('.wishlist-button');
@@ -10,68 +9,81 @@ var moviePlotEL = document.querySelector('.Plot');
 var results = document.querySelector('#results');
 var wishlist = document.querySelector('#wishlist');
 var searchMovie = document.querySelector('#search-movie-bar');
-// add event listener to connect the function so it happens
-// submitbtnEl.click(getmoviedata)
+var upcoming = document.querySelector('#upcoming');
+var carousel = document.querySelectorAll('.carousel-pic');
+
+// event listeners to buttons
 submitbtnEl.addEventListener('click', getmoviedata);
 searchBarBtn.addEventListener('click', searchBarPage);
 wishListBtn.addEventListener('click', wishListPage)
 
 
-// query's citys weather pulls data
-  function getmoviedata(event) {
-    event.preventDefault();
-    results.setAttribute("style", "display");
-    var searchInputEl = $("#search-movie-input")
-    var title = searchInputEl.val()
-    console.log(title);
-    fetch(`https://www.omdbapi.com/?t=${title}&apikey=a8088794`)
-  .then(response => response.json())
-  .then(response => 
-    {
-        console.log(response)
-        var Title = response.Title;
-        var Released = response.Released;
-        var Poster = response.Poster;
-        var Plot = response.Plot;
+// OMDB API fetch for search bar results
+function getmoviedata(event) {
+  event.preventDefault();
+  results.setAttribute("style", "display");
+  var searchInputEl = $("#search-movie-input")
+  var title = searchInputEl.val()
+  console.log(title);
+  fetch(`https://www.omdbapi.com/?t=${title}&apikey=a8088794`)
+    .then(response => response.json())
+    .then(response => {
+      console.log(response)
+      var Title = response.Title;
+      var Released = response.Released;
+      var Poster = response.Poster;
+      var Plot = response.Plot;
 
-       
-        movieTitleEL.textContent = Title;
-        movieReleasedEL.textContent = Released;
-        moviePosterEL.src = Poster;
-        moviePlotEL.textContent = Plot;
-       
-        
+      movieTitleEL.textContent = Title;
+      movieReleasedEL.textContent = Released;
+      moviePosterEL.src = Poster;
+      moviePlotEL.textContent = Plot;
 
+    })
 
+    .catch(err => console.error(err));
+
+}
+
+// TMDB API fetch for upcoming movie results
+fetch('https://api.themoviedb.org/3/movie/upcoming?api_key=dd204bc02415589bf9e5f5b40b5ad914&language=en-US&page=1')
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    for (var i = 0; i < carousel.length; i++) {
+      carousel[i].src = "https://image.tmdb.org/t/p/original/" + data.results[i].poster_path;
     }
-  )
-  .catch(err => console.error(err));
-    
-  }
+  });
 
-  function searchBarPage(event) {
-    event.preventDefault();
-    searchMovie.setAttribute("style", "display");
-    results.setAttribute("style", "display:none");
-    wishlist.setAttribute("style", "display:none");
-  }
+// Rendering serch page
+function searchBarPage(event) {
+  event.preventDefault();
+  searchMovie.setAttribute("style", "display");
+  upcoming.setAttribute("style", "display");
+  results.setAttribute("style", "display:none");
+  wishlist.setAttribute("style", "display:none");
+}
 
-  function wishListPage(event) {
-    event.preventDefault();
-    searchMovie.setAttribute("style", "display:none");
-    results.setAttribute("style", "display:none");
-    wishlist.setAttribute("style", "display");
-  }
+// Rendering watchlist page
+function wishListPage(event) {
+  event.preventDefault();
+  searchMovie.setAttribute("style", "display:none");
+  upcoming.setAttribute("style", "display:none");
+  results.setAttribute("style", "display:none");
+  wishlist.setAttribute("style", "display");
+}
+
 // jQuery for navbar
-
-$(document).ready(function(){
+$(document).ready(function () {
   $('.sidenav').sidenav();
 });
-// jQuery for carousel
 
-$(document).ready(function(){
+// jQuery for carousel
+$(document).ready(function () {
   $('.carousel').carousel();
 });
+
 // scroll to the results
 $('#search-movie').click(function () {
   var offset = $('#results').offset().top;
